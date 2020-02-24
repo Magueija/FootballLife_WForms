19f7ca9a7ab451454e034f7dd499841d57bfa7e8
Tomas Silva <tomas.magueija.silva@gmail.com>
2020-02-24 01:37:24 +0000

    Database error fixed

--- FootballLife_WF/Properties/Settings.Designer.cs
+++ FootballLife_WF/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace FootballLife_WF.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,8 @@ namespace FootballLife_WF.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=FootballLife_DB;Persist Security Info=True;User ID=" +
-            "sa;Password=sa")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-TOMAS\\HOME_SQLSERVER;Initial Catalog=FootballLife_DB;Integrat" +
+            "ed Security=True;")]
         public string Connection {
             get {
                 return ((string)(this["Connection"]));
